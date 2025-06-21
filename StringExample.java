d5461b4b7194a953b1ea4f62b7eda125a517d821
Rushikesh Khare <rushikeshkhare01@gmail.com>
2025-06-21 21:15:38 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,14 @@
+
+
+public class StringExample
+{
+  public static void main(String Arg[])
+  {
+    //String name = new String("Rushi");
+    String name = "Rushi";
+
+    System.out.println("hello " + name);
+    System.out.println(name.charAt(1));
+  }
+    
+}
